38dfbc0d004b14d4e7d4c7f6c7b4b10517c4c483
Ari Liikamaa <ari.liikamaa@gmail.com>
2018-04-02 19:21:43 +0300

    Intial commit

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	// Use this for initialization
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	void Start () {
+		Invoke("LoadStartMenu", 2f);
+	}
+	
+	void LoadStartMenu()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
